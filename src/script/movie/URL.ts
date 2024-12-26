c1f3c59feed717dceacd37dd905659a335441139
sw1kwon <kwonsoonwon0628@gmail.com>
2024-12-26 21:34:16 +0900

    Update URL.ts

--- src/script/movie/URL.ts
+++ src/script/movie/URL.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const fetchFeaturedMovie = async (apiKey: string) => {
+    const apiKey = import.meta.env.TMDB_API_KEY; // 환경 변수에서 TMDB API 키 가져오기
     try {
         const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR`);
         console.log(response.data.results[0]);
@@ -11,14 +12,17 @@ const fetchFeaturedMovie = async (apiKey: string) => {
 }
 
 const getURL4PopularMovies = (apiKey: string, page: number=1) => {
+    const apiKey = import.meta.env.TMDB_API_KEY;
     return `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&page=${page}`;
 }
 
 const getURL4ReleaseMovies = (apiKey: string, page: number=2) => {
+    const apiKey = import.meta.env.TMDB_API_KEY;
     return `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ko-KR&page=${page}`;
 }
 
 const getURL4GenreMovies = (apiKey: string, genre: string, page: number = 1) => {
+    const apiKey = import.meta.env.TMDB_API_KEY;
     return `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}&language=ko-KR&page=${page}`;
 }
 
